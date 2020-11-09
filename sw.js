c573da752e562dbf4462b9336bc9826684f638b5
Sergen Tufanoğulları <sergen@whitehorse.technology>
2020-11-09 12:59:58 +0300

    test

---
+++ sw.js
@@ -0,0 +1,13 @@
+var wis_push_default_company_id = '1566';
+var wis_push_tracking_url = '//dengage.webinstats.com/';
+var wis_payload_vars = {title:'t',body:'b',icon:'i',link:'l',image:'m',vibrate:'vibrate',actions:'actions',actiontitle:'title',actionicon:'icon',actionlink:'link',actionclickid:'clickid',actionaction:'action',wispushcollapse:'wisclps'};
+var wis_ss_cookie = '0';
+var wis_default_zid = '25552';
+
+var wis_default_title='dEngage';
+var wis_default_body='A new era in customer engagement !';
+var wis_default_domain = 'https://dengage-showcase.github.io/';
+var wis_default_icon='//dbfukofby5ycr.cloudfront.net/m47/1/1566/d2011/e395a-dengage.png';
+
+
+(typeof importScripts === "function")  && importScripts("https://dbfukofby5ycr.cloudfront.net/a13/swv3.js");
